a1b402a9a156eb3c66eb159b45c10e5241881995
Tomasz Kulka <kulkejszyn@gmail.com>
2020-10-28 18:53:19 +0100

    feat(cwiczenie4)

--- cwiczenie.sql
+++ cwiczenie.sql
@@ -1,98 +1,69 @@
--- 3pos
-CREATE TABLE budynki
-(
-    id        SERIAL PRIMARY KEY,
-    geometria GEOMETRY,
-    nazwa     VARCHAR(50)
-);
-CREATE TABLE drogi
-(
-    id        SERIAL PRIMARY KEY,
-    geometria GEOMETRY,
-    nazwa     VARCHAR(50)
-);
-CREATE TABLE punkty_informacyjne
-(
-    id        SERIAL PRIMARY KEY,
-    geometria GEOMETRY,
-    nazwa     VARCHAR(50)
-);
+-- 1-3 Import przez gui
+
+-- 4. Wyznacz liczbę budynków (tabela: popp, atrybut: f_codedesc, reprezentowane, jako punkty)
+-- położonych w odległości mniejszej niż 100000 m od głównych rzek. Budynki spełniające to
+-- kryterium zapisz do osobnej tabeli tableB
+
+CREATE TABLE tableB AS
+SELECT popp.*
+FROM popp,
+     majrivers
+WHERE popp.f_codedesc = 'Building'
+GROUP BY popp.gid
+HAVING MIN(ST_Distance(majrivers.geom, popp.geom)) < 100000;
+
+SELECT COUNT(1)
+FROM tableB;
 
 
--- 4
+-- 5. Utwórz tabelę o nazwie airportsNew. Z tabeli airports do airportsNew zaimportuj nazwy lotnisk,
+-- ich geometrię, a także atrybut elev, reprezentujący wysokość n.p.m.
+CREATE TABLE airportsNew AS
+SELECT name, geom, elev
+FROM airports;
+-- a) Znajdź lotnisko, które położone jest najbardziej na zachód i najbardziej na wschód.
 
-INSERT INTO budynki(geometria, nazwa)
-VALUES (st_geogfromtext('POLYGON((10.5 4,10.5 1.5, 8 1.5,8 4, 10.5 4))'), 'BuildingA'),
-       (st_geogfromtext('POLYGON((6 7, 6 5, 4 5, 4 7, 6 7))'), 'BuildingB'),
-       (st_geogfromtext('POLYGON((5 8, 5 6, 3 6, 3 8, 5 8))'), 'BuildingC'),
-       (st_geogfromtext('POLYGON((10 9, 10 8, 9 8, 9 9, 10 9))'), 'BuildingD'),
-       (st_geogfromtext('POLYGON((2 2, 2 1, 1 1, 1 2, 2 2))'), 'BuildingF');
-
-INSERT INTO drogi(geometria, nazwa)
-VALUES (st_makeline(st_makeline('POINT(0 4.5)'), st_makeline('POINT(12 4.5)')), 'RoadX'),
-       (st_makeline(st_makeline('POINT(7.5 0)'), st_makeline('POINT(12 10.5)')), 'RoadY');
-
-INSERT INTO punkty_informacyjne(geometria, nazwa)
-VALUES (st_geogfromtext('POINT(1 3.5)'), 'G'),
-       (st_geogfromtext('POINT(5.5 1.5)'), 'H'),
-       (st_geogfromtext('POINT(9.5 6)'), 'I'),
-       (st_geogfromtext('POINT(6.5 6)'), 'J'),
-       (st_geogfromtext('POINT(6 9.5)'), 'K');
-
--- ST_Length, ST_Area, ST_Distance, ST_Buffer, ST_Intersection, ST_GeomFromText,
--- ST_Contains, ST_X, ST_Y
-
--- a. Wyznacz całkowitą długość dróg w analizowanym mieście.
-SELECT SUM(st_length(geometria))
-FROM drogi;
-
--- b. Wypisz geometrię (WKT), pole powierzchni oraz obwód poligonu reprezentującego
--- budynek o nazwie BuildingA.
-SELECT st_asewkt(geometria), st_area(geometria), st_perimeter(geometria)
-FROM budynki
-WHERE nazwa LIKE '%A';
-
--- c. Wypisz nazwy i pola powierzchni wszystkich poligonów w warstwie budynki. Wyniki
--- posortuj alfabetycznie.
-SELECT nazwa, st_area(geometria)
-FROM budynki
-ORDER BY nazwa;
-
--- d. Wypisz nazwy i obwody 2 budynków o największej powierzchni.
-SELECT nazwa, st_perimeter(geometria)
-FROM budynki
-ORDER BY st_area(geometria) DESC
-LIMIT 2;
-
--- e. Wyznacz najkrótszą odległość między budynkiem BuildingC a punktem G.
-SELECT st_distance(budynki.geometria, punkty_informacyjne.geometria)
-FROM budynki,
-     punkty_informacyjne
-WHERE budynki.nazwa LIKE '%C'
-  and punkty_informacyjne.nazwa = 'G';
-
--- f. Wypisz pole powierzchni tej części budynku BuildingC, która znajduje się w odległości
--- większej niż 0.5 od budynku BuildingB.
--- SELECT SELECT st_area(st_difference(
---         st_buffer((SELECT geometria FROM budynki WHERE nazwa LIKE '%B'), 0.5),
---         (SELECT geometria FROM budynki WHERE nazwa LIKE '%C')
---     ));
-
-SELECT st_area(st_intersection(
-        st_buffer((SELECT geometria FROM budynki WHERE nazwa LIKE '%B'), 0.5),
-        (SELECT geometria FROM budynki WHERE nazwa LIKE '%C')
-    ));
-
--- g. Wybierz te budynki, których centroid (ST_Centroid) znajduje się powyżej drogi
---  o nazwie RoadX.
+SELECT MIN(ST_Y(geom)), MAX(ST_Y(geom))
+FROM airportsNew;
+--zachod
+CREATE VIEW zachod AS
+SELECT *
+FROM airportsNew
+ORDER BY ST_Y(geom) DESC
+LIMIT 1;
+--wschod
+CREATE VIEW wschod AS
+SELECT *
+FROM airportsNew
+ORDER BY ST_Y(geom)
+LIMIT 1;
 
 SELECT *
-FROM budynki
-WHERE ST_Y(ST_Centroid(geometria)) > ST_Y(ST_PointN((SELECT geometria FROM drogi WHERE nazwa LIKE 'RoadX'), 1));
+FROM wschod;
+SELECT *
+FROM zachod;
 
--- 8. Oblicz pole powierzchni tych części budynku BuildingC i poligonu o współrzędnych (4 7, 6 7, 6 8, 4 8, 4 7), które nie są wspólne dla tych dwóch obiektów
-SELECT ST_Area(ST_SymDifference(
-            (SELECT geometria FROM budynki WHERE nazwa LIKE 'BuildingC'),
-            ST_GeomFromText('POLYGON(( 4 7, 6 7, 6  8, 4 8, 4 7))'))
-           );
+-- b) Do tabeli airportsNew dodaj nowy obiekt - lotnisko, które położone jest w punkcie
+-- środkowym drogi pomiędzy lotniskami znalezionymi w punkcie a. Lotnisko nazwij airportB.
+-- Wysokość n.p.m. przyjmij dowolną.
 
+SELECT ST_Centroid(ST_MakeLine((SELECT geom FROM wschod), (SELECT geom FROM zachod)));
+INSERT INTO airportsNew (name, geom, elev)
+VALUES ('airportB',
+        (SELECT ST_Centroid(ST_MakeLine((SELECT geom FROM wschod), (SELECT geom FROM zachod)))),
+        12);
+-- 6. Wyznacz pole powierzchni obszaru, który oddalony jest mniej niż 1000 jednostek od najkrótszej
+-- linii łączącej jezioro o nazwie ‘Iliamna Lake’ i lotnisko o nazwie „AMBLER”
+SELECT ST_Area(ST_Buffer(ST_ShortestLine(
+                                     (SELECT geom FROM lakes WHERE names LIKE 'Iliamna Lake'),
+                                     (SELECT geom FROM airports WHERE name LIKE 'AMBLER')
+                             ), 1000));
+
+-- 7. Napisz zapytanie, które zwróci sumaryczne pole powierzchni poligonów reprezentujących
+-- poszczególne typy drzew znajdujących się na obszarze tundry i bagien.
+
+SELECT sum(st_area(trees.geom)), trees.cat
+FROM trees
+         RIGHT JOIN tundra t ON trees.cat = t.cat
+         RIGHT JOIN swamp s ON s.cat = s.cat
+GROUP BY trees.cat;
